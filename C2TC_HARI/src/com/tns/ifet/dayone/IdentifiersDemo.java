8b157d74e2329e6e95049de4a2547bd184e952f8
harir <harir@Hariram_2004>
2024-10-25 16:14:29 +0530

    uploaded code

---
+++ C2TC_HARI/src/com/tns/ifet/dayone/IdentifiersDemo.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.dayone;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $num=20;
+		System.out.println("Values of the number variable is:"+$num);
+		String studentName="Aniket";
+		System.out.println("Value is:"+studentName);
+
+	}
+
+}
---
+++ C2TC_HARI/src/com/tns/ifet/daytwo/IfElseDemo.java
@@ -0,0 +1,16 @@
+package com.tns.ifet.daytwo;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=11;
+		if(age>=18)
+			System.out.println("you ar eligible for voting");
+		else
+			System.out.println("you are not eligible for voting");
+			System.out.println("if else demostration");
+
+	}
+
+}
