c7d207a6da606e5c66b53470e6591f27da290726
harir <harir@Hariram_2004>
2024-11-04 15:44:30 +0530

    Day3

---
+++ C2TC_HARI/src/com/tns/ifet/day3/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.day3.encapsulation;
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsConceptDemo obj = new OopsConceptDemo(); 
+		obj.setSerialNum(4211); 
+		obj.setName("Hariram"); 
+		obj.setAge(21); 
+		System.out.println(obj);
+
+	}
+
+}
