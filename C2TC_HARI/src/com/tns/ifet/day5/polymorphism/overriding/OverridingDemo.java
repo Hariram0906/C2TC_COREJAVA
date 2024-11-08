87476441cee5a14522fb527df5f47cc6a7fee897
harir <harir@Hariram_2004>
2024-11-08 14:43:20 +0530

    day5

---
+++ C2TC_HARI/src/com/tns/ifet/day5/polymorphism/overriding/OverridingDemo.java
@@ -0,0 +1,16 @@
+package com.tns.ifet.day5.polymorphism.overriding;
+
+public class OverridingDemo {
+
+	public static void main(String[] args) {
+		RBI rbi;
+		rbi = new SBI();
+		System.out.println(rbi.getRateOfInterest());
+		rbi = new ICICI();
+		System.out.println(rbi.getRateOfInterest());
+		rbi = new HDFC();
+		System.out.println(rbi.getRateOfInterest());
+
+	}
+
+}
